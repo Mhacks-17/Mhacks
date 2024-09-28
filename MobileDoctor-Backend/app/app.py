1464f1214a3d02c73c5de44f4c833e26a7c422f3
khedekar <64322243+titan-6@users.noreply.github.com>
2024-09-28 19:37:03 -0400

    changes

--- MobileDoctor-Backend/app/app.py
+++ MobileDoctor-Backend/app/app.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from inference_sdk import InferenceHTTPClient
 
 app = FastAPI()
+
+client = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="oCNXt21BQhz9229Qrev8"
+)
 
 @app.get("/")
 async def read_root():
@@ -9,3 +15,15 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id, "description": "This is an item."}
+
+@app.post("/upload-image/")
+async def upload_image(file: UploadFile = File(...)):
+    contents = await file.read()  # Read the uploaded file
+    result = client.run_workflow(
+        workspace_name="music-5umoc",
+        workflow_id="custom-workflow",
+        images={
+            "Body": contents  # Use the file contents directly
+        }
+    )
+    return result
